73459d261d2af6dac7980b2981cae82fe22a3a30
NikitaJagtap2003 <jagtapnikita541@gmail.com>
2025-02-11 01:01:40 +0530

    cloths added

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const WEBSITE_NAME = "mahalakshmi Kurti  Wear👗";
+const CARD_TITLE = "Black Rayon Kurti For Navratri Garba Festival ";
+const PRODUCT_PRICE ="₹500"
+const MAIN_IMG ="https://www.pomchajaipur.com/cdn/shop/products/1_7.jpg?v=1670337807&width=1080"
+const SUB_IMG ="https://www.pomchajaipur.com/cdn/shop/products/image_f621a051-8d1e-4eac-aa93-ac451bad89e0.jpg?v=1671802089&width=1800"
+const SUB_IMG1 ="https://www.pomchajaipur.com/cdn/shop/products/1_7.jpg?v=1670337807&width=1080"
+const SUB_IMG2 ="https://www.pomchajaipur.com/cdn/shop/products/image_f621a051-8d1e-4eac-aa93-ac451bad89e0.jpg?v=1671802089&width=1800"
+const SUB_IMG3 ="https://www.pomchajaipur.com/cdn/shop/products/2_8_2b674df5-5a9e-4bde-8db5-105dacd0c759.jpg?v=1670337807&width=1800"
+
+export {WEBSITE_NAME,CARD_TITLE, PRODUCT_PRICE,MAIN_IMG,SUB_IMG,SUB_IMG1,SUB_IMG2,SUB_IMG3};
